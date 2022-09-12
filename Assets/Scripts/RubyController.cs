386ee589768c1310dc011e7f0605c0c06d728c1e
noobcoder-98 <anoobcoder98@gmail.com>
2022-09-12 22:35:23 +0700

    feat: character controller and keyboard input

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -7,14 +7,20 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //QualitySettings.vSyncCount = 0;
+        //Application.targetFrameRate = 10;
     }
 
     // Update is called once per frame
     void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Debug.Log(horizontal);
+        Debug.Log(vertical);
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + 3f * horizontal * Time.deltaTime;
+        position.y = position.y + 3f * vertical * Time.deltaTime;
         transform.position = position;
     }
 }

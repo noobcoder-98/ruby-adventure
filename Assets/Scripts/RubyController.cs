2eab396883d2596b85ee65cd960c7ba04671c51c
noobcoder-98 <anoobcoder98@gmail.com>
2022-09-13 17:27:36 +0700

    feat: collectible

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public float speed = 3.0f;
+
+    public int maxHealth = 5;
+
+    int currentHealth;
+    public int health { get { return currentHealth; } }
+
     Rigidbody2D rigidbody2D;
     float horizontal;
     float vertical;
@@ -11,12 +18,13 @@ public class RubyController : MonoBehaviour
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth - 1;
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 
     // Update is called once per frame
     void Update()
     {
-
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
     }
@@ -24,8 +32,14 @@ public class RubyController : MonoBehaviour
     private void FixedUpdate()
     {
         Vector2 position = rigidbody2D.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
         rigidbody2D.MovePosition(position);
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
